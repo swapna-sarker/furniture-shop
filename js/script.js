87128845134e6ce1376ffd834334c50f02addfc6
swapna-sarker <souravsarker580@gmail.com>
2024-09-24 17:18:11 +0600

    1st commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function addNumber( number1,number2) {
+    const totalNumber = number1 + number2;
+    console.log ("Your Additional Number Is = " + totalNumber)
+}
+
+addNumber(600,200);
+
+function subNumber( number1,number2) {
+    const totalNumber = number1 - number2;
+    console.log ("Your sub Number Is = " + totalNumber)
+}
+
+subNumber(800,300);
+
+
+
+function multipulNumber( number1,number2) {
+    const totalNumber = number1 * number2;
+    console.log ("Your multipul Number Is = " + totalNumber)
+}
+
+multipulNumber(250,100);
+
+
+function devideNumber( number1,number2) {
+    const totalNumber = number1 / number2;
+    console.log ("Your Devidetion Number Is = " + totalNumber)
+}
+
+devideNumber(300,150);
